a10e6034d3e33066893ba018bbc41a5b0cbc4d07
gom_55@naver.com <gom_55@naver.com>
2021-04-26 10:15:49 +0900

    4월 26일 에러페이지 제작

---
+++ spring_project/src/main/java/kr/kjy/model/SampleDTO.java
@@ -0,0 +1,11 @@
+package kr.kjy.model;
+
+import lombok.Data;
+
+@Data
+public class SampleDTO {
+	
+	private String name;
+	private int age;
+	
+}
--- spring_project/src/main/java/kr/kjy/myapp/SampleController.java
+++ spring_project/src/main/java/kr/kjy/myapp/SampleController.java
@@ -7,17 +7,22 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.multipart.MultipartFile;
 
+import kr.kjy.model.SampleDTO;
 import lombok.extern.log4j.Log4j;
 
 @Log4j
 @Controller
 public class SampleController {
 	
+	@GetMapping("/ex01")
+	public void ex01(SampleDTO dto) {
+		log.info(dto);
+	}
+	
 	@GetMapping("/exUpload")
 	public void exUpload() {
 		log.info("exUpload...........");
 	}
-	
 	
 	@PostMapping("/exUploadPost")
 	public void exUploadPost(ArrayList<MultipartFile> files) {
